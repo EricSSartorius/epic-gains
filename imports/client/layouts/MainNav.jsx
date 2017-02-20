db1a31d69940eae4ac6b263b2d12a36e5ee849ae
EricSSartorius <EricSSartorius@gmail.com>
2017-02-20 11:28:15 +0400

    new pages added

--- imports/client/layouts/MainNav.jsx
+++ imports/client/layouts/MainNav.jsx
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-
 import AccountsUIWrapper from '../utilities/AccountsUIWrapper';
 
 const MainNav = () =>
   <nav className="main-nav">
-    {Meteor.userId() ? (
+    {/* {Meteor.userId() ? ( */}
       <ul>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/workout'>Workouts</Link></li>
         <li><Link to='/exercise'>Exercises</Link></li>
         <li><Link to='/timer'>Timer</Link></li>
         {/* <li><Link to='/library'>Library</Link></li> */}
+        {/* <li><Link to='/teams'>Teams</Link></li> */}
+        {/* <li><Link to='/settings'>Settings</Link></li> */}
         <li><AccountsUIWrapper /></li>
       </ul>
-    ) : (
+    {/* ) : (
       <ul>
         <li>
           <AccountsUIWrapper />
         </li>
       </ul>
-    )}
+    )} */}
   </nav>
 
 export default MainNav;
